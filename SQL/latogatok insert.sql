7fcbf6199d949a2f8ebb360c5fd4b973477f9259
Rapcsak12 <78409938+Rapcsak12@users.noreply.github.com>
2025-02-17 14:42:30 +0100

    asd

--- SQL/latogatok insert.sql
+++ SQL/latogatok insert.sql
@@ -1,5 +1,5 @@
-INSERT INTO latogatok (vnev, knev, knev2, telefonszam, szul_ido, lakhelyvaros, regisztracio_datum, felhasznalonev, jelszo, edzo)  
+INSERT INTO latogatok (user_id, vnev, knev, knev2, telefonszam, szul_ido, lakhelyvaros, regisztracio_datum, felhasznalonev, jelszo, edzo)  
 VALUES 
-('Kelemen', 'Zoltán', NULL, '06101245424', '2000-11-20 10:30:20', 'Budapest', '2025-02-16 22:33:15', 'kelzoli', 'titkosjelszo', 0),
-('Lakatos', 'Erik', 'Ferenc', '06102413111', '1987-03-10 04:44:21', 'Budapest', '2025-02-16 22:35:00', 'Erik', 'titkosjelszo', 0),
-('Kiss', 'Péter', NULL, '06101234567', '1995-06-15 00:00:00', 'Budapest', '2025-02-16 22:30:19', 'kisspeter', 'titkosjelszo', 1);
+('1', 'Kelemen', 'Zoltán', NULL, '06101245424', '2000-11-20 10:30:20', 'Budapest', '2025-02-16 22:33:15', 'kelzoli', 'titkosjelszo', 0),
+('2', 'Lakatos', 'Erik', 'Ferenc', '06102413111', '1987-03-10 04:44:21', 'Budapest', '2025-02-16 11:05:22', 'Erik', 'titkosjelszo', 0),
+('3', 'Kiss', 'Péter', NULL, '06101234567', '1995-06-15 03:00:20', 'Budapest', '2025-02-16 5:30:19', 'kisspeter', 'titkosjelszo', 1);
