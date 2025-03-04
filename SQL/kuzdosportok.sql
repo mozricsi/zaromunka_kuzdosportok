e2263a6b362a46c204c33fa3455cc5bf8c4c8c4a
mozricsi <mozric721@hengersor.hu>
2025-03-04 10:26:42 +0100

    sdsda

--- SQL/kuzdosportok.sql
+++ SQL/kuzdosportok.sql
@@ -2,190 +2,216 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Már 04. 08:45
--- Kiszolgáló verziója: 10.4.32-MariaDB
--- PHP verzió: 8.2.12
+-- Host: 127.0.0.1
+-- Generation Time: 2025. Mar 04 08:45
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
+-- Set character set and collation
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Adatbázis: `kuzdosportok`
+-- Database: `kuzdosportok`
 --
-<<<<<<< HEAD:SQL/kuzdosportok.sql
-=======
-
->>>>>>> dd23d03225a308e608d5c117ad69253058244b5b:SQL/kuzdosportok (4).sql
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `adatmodositas`
+-- Table structure for table `adatmodositas`
 --
 
 CREATE TABLE `adatmodositas` (
-  `modositas_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `adat_modositas_datum` datetime NOT NULL
+  `modositas_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `adat_modositas_datum` DATETIME NOT NULL,
+  PRIMARY KEY (`modositas_id`),
+  KEY `fr_user_id_idx` (`user_id`),
+  CONSTRAINT `fk_adatmodositas_user` FOREIGN KEY (`user_id`) REFERENCES `latogatok` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `ertekelesek`
+-- Table structure for table `ertekelesek`
 --
 
 CREATE TABLE `ertekelesek` (
-  `ertekeles_id` int(11) NOT NULL,
-  `szoveges_ertekeles` varchar(255) NOT NULL,
-  `csillagos_ertekeles` int(1) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `sportklub_id` int(11) NOT NULL
+  `ertekeles_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `szoveges_ertekeles` VARCHAR(255) NOT NULL,
+  `csillagos_ertekeles` INT(1) NOT NULL,
+  `user_id` INT(11) NOT NULL,
+  `sportklub_id` INT(11) NOT NULL,
+  PRIMARY KEY (`ertekeles_id`),
+  KEY `fr_user_id_idx` (`user_id`),
+  KEY `fr_sportklub_idx` (`sportklub_id`),
+  CONSTRAINT `fk_ertekelesek_user` FOREIGN KEY (`user_id`) REFERENCES `latogatok` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_ertekelesek_sportklub` FOREIGN KEY (`sportklub_id`) REFERENCES `klubbok` (`sportklub_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `esemenyek`
+-- Table structure for table `esemenyek`
 --
 
 CREATE TABLE `esemenyek` (
-  `esemeny_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `latogato_resztvevo` int(1) NOT NULL,
-  `pontos_cim` varchar(255) NOT NULL,
-  `ido` datetime NOT NULL,
-  `sportneve` varchar(255) NOT NULL,
-  `leiras` varchar(255) NOT NULL,
-  `szervezo_neve` varchar(255) NOT NULL,
-  `szervezo_tel` varchar(15) NOT NULL,
-  `szervezo_email` varchar(255) NOT NULL,
-  `esemeny_weboldal` varchar(255) DEFAULT NULL
+  `esemeny_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `latogato_resztvevo` INT(1) NOT NULL,
+  `pontos_cim` VARCHAR(255) NOT NULL,
+  `ido` DATETIME NOT NULL,
+  `sportneve` VARCHAR(255) NOT NULL,
+  `leiras` VARCHAR(255) NOT NULL,
+  `szervezo_neve` VARCHAR(255) NOT NULL,
+  `szervezo_tel` VARCHAR(15) NOT NULL,
+  `szervezo_email` VARCHAR(255) NOT NULL,
+  `esemeny_weboldal` VARCHAR(255) DEFAULT NULL,
+  PRIMARY KEY (`esemeny_id`),
+  KEY `fr_user_id_idx` (`user_id`),
+  CONSTRAINT `fk_esemenyek_user` FOREIGN KEY (`user_id`) REFERENCES `latogatok` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `jelentkezes`
+-- Table structure for table `jelentkezes`
 --
 
 CREATE TABLE `jelentkezes` (
-  `jelentkezes_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `sportkulb_id` int(11) NOT NULL,
-  `jelentkezes_ido` datetime NOT NULL,
-  `elfogadasi_ido` datetime NOT NULL,
-  `elfogadva` int(1) NOT NULL
+  `jelentkezes_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `sportklub_id` INT(11) NOT NULL,
+  `jelentkezes_ido` DATETIME NOT NULL,
+  `elfogadasi_ido` DATETIME NOT NULL,
+  `elfogadva` INT(1) NOT NULL,
+  PRIMARY KEY (`jelentkezes_id`),
+  KEY `fk_user_id_idx` (`user_id`),
+  KEY `fk_sportklub_id_idx` (`sportklub_id`),
+  CONSTRAINT `fk_jelentkezes_user` FOREIGN KEY (`user_id`) REFERENCES `latogatok` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_jelentkezes_sportklub` FOREIGN KEY (`sportklub_id`) REFERENCES `klubbok` (`sportklub_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `klubbok`
+-- Table structure for table `klubbok`
 --
 
 CREATE TABLE `klubbok` (
-  `sprotklub_id` int(11) NOT NULL,
-  `sport_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `vnev` varchar(255) NOT NULL,
-  `knev` varchar(255) NOT NULL,
-  `klubbnev` varchar(255) NOT NULL,
-  `hely` varchar(255) NOT NULL,
-  `idonap` varchar(255) NOT NULL,
-  `ido` time NOT NULL,
-  `szabalyok` text DEFAULT NULL,
-  `leiras` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
---
--- A tábla adatainak kiíratása `klubbok`
---
-
-INSERT INTO `klubbok` (`sprotklub_id`, `sport_id`, `user_id`, `vnev`, `knev`, `klubbnev`, `hely`, `idonap`, `ido`, `szabalyok`, `leiras`) VALUES
-(1, 1, 1, 'Kiss', 'Péter', 'Szolnoki Boxegyesület', 'Szolnok', 'Hétfő', '17:30:00', 'Nincs szabály', 'Barátságos közösség vagyunk.'),
-(2, 1, 2, 'Nagy', 'Anna', 'Debreceni Boxegyesület', 'Debrecen', 'Szerda', '11:30:00', 'Nincs szabály', 'Barátságos közösség vagyunk.'),
-(3, 2, 3, 'Tóth', 'Gábor', 'Szolnoki Judo egyesület', 'Szeged', 'Péntek', '20:00:00', 'Nincs szabály', 'Barátságos közösség vagyunk.'),
-(4, 2, 7, 'bela', 'feri', 'bferi Klubja', 'Vecsés', 'Hétfő', '16:28:00', NULL, 'halado'),
-(8, 8, 7, 'bela', 'feri', 'bferi Klubja', 'asaa', 'Kedd', '08:46:00', NULL, 'Nincs leírás megadva');
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `latogatobejelentkezesek`
---
-
-CREATE TABLE `latogatobejelentkezesek` (
-  `bejelentkezes_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `bejelentkezes_ido` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
---
--- A tábla adatainak kiíratása `latogatobejelentkezesek`
---
-
-INSERT INTO `latogatobejelentkezesek` (`bejelentkezes_id`, `user_id`, `bejelentkezes_ido`) VALUES
-(0, 4, '2025-03-04 08:44:44');
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `latogatok`
---
-
-CREATE TABLE `latogatok` (
-  `user_id` int(11) NOT NULL,
-  `vnev` varchar(255) NOT NULL,
-  `knev` varchar(255) NOT NULL,
-  `knev2` varchar(255) DEFAULT NULL,
-  `telefonszam` int(100) DEFAULT NULL,
-  `email` varchar(255) NOT NULL,
-  `szul_ido` date NOT NULL,
-  `lakhelyvaros` varchar(255) NOT NULL,
-  `regisztracio_datum` datetime NOT NULL,
-  `felhasznalonev` varchar(255) NOT NULL,
-  `jelszo` varchar(255) NOT NULL,
-  `role` enum('visitor','coach') DEFAULT 'visitor'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
---
--- A tábla adatainak kiíratása `latogatok`
---
-
-INSERT INTO `latogatok` (`user_id`, `vnev`, `knev`, `knev2`, `telefonszam`, `email`, `szul_ido`, `lakhelyvaros`, `regisztracio_datum`, `felhasznalonev`, `jelszo`, `role`) VALUES
-(1, 'Kiss', 'Péter', NULL, 1, 'kiss.peter@example.com', '1990-05-12', 'Budapest', '2025-02-22 00:00:00', 'kpeter', '$2b$10$Ye0uo3ffgRUMuM1rX369V.6di9clKRXre7/vjxOz5u3QlYDUxWlIW', 'visitor'),
-(2, 'Nagy', 'Anna', NULL, 2, 'nagy.anna@example.com', '1985-08-25', 'Debrecen', '2025-02-22 00:00:00', 'nanna', '$2b$10$F6z0jKRpuk5Iw9X/Hd5FRuG.ADcQmEEOd0Fsqci1jVXz2vZhUXzQi', 'visitor'),
-(3, 'Tóth', 'Gábor', 'Ferenc', 3, 'toth.gabor@example.com', '1992-11-10', 'Szeged', '2025-02-22 00:00:00', 'tgabor', '$2b$10$NTAJ.Zz7abNQN44PPYo2yOLxmWP0BXy3n/KZa6b16KTgCbD7J7MfG', 'visitor'),
-(4, 'Szabó', 'Mária', 'Anna', 4, 'szabo.maria@example.com', '1995-03-14', 'Pécs', '2025-02-22 00:00:00', 'smaria', '$2b$10$lU4EEgGIuS2QhocVvFBi4eekNPi.sDCCYKqWC1.z1LarzOC23A.HG', 'visitor'),
-(5, 'Varga', 'Béla', NULL, 5, 'varga.bela@example.com', '1988-07-19', 'Győr', '2025-02-22 00:00:00', 'vbela', '$2b$10$S/oxPWR1GAefsAw1sksUtu9ghwQO.M1d9pOiQ3vX0Q5uQHSfbNqV2', 'visitor'),
-(6, 'kovacs', 'jeno', NULL, NULL, 'kovacsjeno@gmail.com', '2025-03-19', 'Saab', '2025-03-03 12:58:26', 'kjeno', '$2b$10$E4uY7aLRJ4uCeMCXKvDOj.Z6MzmiHVAm4mZKRLqhMcNywp8ihg8ou', 'visitor'),
-(7, 'bela', 'feri', NULL, NULL, 'belaferi@gmail.com', '2025-03-14', 'Budapest', '2025-03-03 14:09:27', 'bferi', '$2b$10$I/6jCKKXuCCqpoPBu7Qcy.fLhOjeaCYOpgTx267Eu3/Rzv7CyHTZu', 'coach'),
-(8, 'kati', 'bela', NULL, NULL, 'katibela@gmail.com', '2025-03-27', 'Szeged', '2025-03-03 14:10:52', 'kbela', '$2b$10$fBN1L4viZ4imJqPtzm61vOSBHLbyeLwsJsjv45h0sda/u7QTr2PSy', 'visitor');
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `sport`
---
-
-CREATE TABLE `sport` (
-  `sport_id` int(11) NOT NULL,
-  `sportnev` varchar(45) DEFAULT NULL,
-  `leiras` varchar(255) DEFAULT NULL,
-  `szabalyok` varchar(555) DEFAULT NULL
+  `sportklub_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `sport_id` INT(11) NOT NULL,
+  `user_id` INT(11) NOT NULL,
+  `vnev` VARCHAR(255) NOT NULL,
+  `knev` VARCHAR(255) NOT NULL,
+  `klubbnev` VARCHAR(255) NOT NULL,
+  `hely` VARCHAR(255) NOT NULL,
+  `idonap` VARCHAR(255) NOT NULL,
+  `ido` TIME NOT NULL,
+  `szabalyok` TEXT DEFAULT NULL,
+  `leiras` TEXT DEFAULT NULL,
+  PRIMARY KEY (`sportklub_id`),
+  KEY `fk_user_id_idx` (`user_id`),
+  KEY `sport_id_idx` (`sport_id`),
+  KEY `kb_vnev_idx` (`vnev`),
+  KEY `kb_knev_idx` (`knev`),
+  CONSTRAINT `fk_klubbok_user` FOREIGN KEY (`user_id`) REFERENCES `latogatok` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_klubbok_sport` FOREIGN KEY (`sport_id`) REFERENCES `sport` (`sport_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- A tábla adatainak kiíratása `sport`
+-- Dumping data for table `klubbok`
+--
+
+INSERT INTO `klubbok` (`sportklub_id`, `sport_id`, `user_id`, `vnev`, `knev`, `klubbnev`, `hely`, `idonap`, `ido`, `szabalyok`, `leiras`) VALUES
+(1, 1, 1, 'Kiss', 'Péter', 'Szolnoki Boxegyesület', 'Szolnok', 'Hétfő', '17:30:00', 'Nincs szabály', 'Barátságos közösség vagyunk.'),
+(2, 1, 2, 'Nagy', 'Anna', 'Debreceni Boxegyesület', 'Debrecen', 'Szerda', '11:30:00', 'Nincs szabály', 'Barátságos közösség vagyunk.'),
+(3, 2, 3, 'Tóth', 'Gábor', 'Szolnoki Judo egyesület', 'Szeged', 'Péntek', '20:00:00', 'Nincs szabály', 'Barátságos közösség vagyunk.'),
+(4, 2, 7, 'Bela', 'Feri', 'Bferi Klubja', 'Vecsés', 'Hétfő', '16:28:00', NULL, 'Haladó'),
+(8, 8, 7, 'Bela', 'Feri', 'Bferi Klubja', 'Asaa', 'Kedd', '08:46:00', NULL, 'Nincs leírás megadva');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `latogatobejelentkezesek`
+--
+
+CREATE TABLE `latogatobejelentkezesek` (
+  `bejelentkezes_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `user_id` INT(11) NOT NULL,
+  `bejelentkezes_ido` DATETIME NOT NULL,
+  PRIMARY KEY (`bejelentkezes_id`),
+  KEY `fk_latogatobejelentkezesek_latogatok` (`user_id`),
+  CONSTRAINT `fk_latogatobejelentkezesek_user` FOREIGN KEY (`user_id`) REFERENCES `latogatok` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `latogatobejelentkezesek`
+--
+
+INSERT INTO `latogatobejelentkezesek` (`bejelentkezes_id`, `user_id`, `bejelentkezes_ido`) VALUES
+(1, 4, '2025-03-04 08:44:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `latogatok`
+--
+
+CREATE TABLE `latogatok` (
+  `user_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `vnev` VARCHAR(255) NOT NULL,
+  `knev` VARCHAR(255) NOT NULL,
+  `knev2` VARCHAR(255) DEFAULT NULL,
+  `telefonszam` VARCHAR(20) DEFAULT NULL, -- Changed to VARCHAR for phone numbers
+  `email` VARCHAR(255) NOT NULL,
+  `szul_ido` DATE NOT NULL,
+  `lakhelyvaros` VARCHAR(255) NOT NULL,
+  `regisztracio_datum` DATETIME NOT NULL,
+  `felhasznalonev` VARCHAR(255) NOT NULL,
+  `jelszo` VARCHAR(255) NOT NULL,
+  `role` ENUM('visitor', 'coach') DEFAULT 'visitor',
+  PRIMARY KEY (`user_id`),
+  KEY `vnev_idx` (`vnev`),
+  KEY `knev_idx` (`knev`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `latogatok`
+--
+
+INSERT INTO `latogatok` (`user_id`, `vnev`, `knev`, `knev2`, `telefonszam`, `email`, `szul_ido`, `lakhelyvaros`, `regisztracio_datum`, `felhasznalonev`, `jelszo`, `role`) VALUES
+(1, 'Kiss', 'Péter', NULL, '1', 'kiss.peter@example.com', '1990-05-12', 'Budapest', '2025-02-22 00:00:00', 'kpeter', '$2b$10$Ye0uo3ffgRUMuM1rX369V.6di9clKRXre7/vjxOz5u3QlYDUxWlIW', 'visitor'),
+(2, 'Nagy', 'Anna', NULL, '2', 'nagy.anna@example.com', '1985-08-25', 'Debrecen', '2025-02-22 00:00:00', 'nanna', '$2b$10$F6z0jKRpuk5Iw9X/Hd5FRuG.ADcQmEEOd0Fsqci1jVXz2vZhUXzQi', 'visitor'),
+(3, 'Tóth', 'Gábor', 'Ferenc', '3', 'toth.gabor@example.com', '1992-11-10', 'Szeged', '2025-02-22 00:00:00', 'tgabor', '$2b$10$NTAJ.Zz7abNQN44PPYo2yOLxmWP0BXy3n/KZa6b16KTgCbD7J7MfG', 'visitor'),
+(4, 'Szabó', 'Mária', 'Anna', '4', 'szabo.maria@example.com', '1995-03-14', 'Pécs', '2025-02-22 00:00:00', 'smaria', '$2b$10$lU4EEgGIuS2QhocVvFBi4eekNPi.sDCCYKqWC1.z1LarzOC23A.HG', 'visitor'),
+(5, 'Varga', 'Béla', NULL, '5', 'varga.bela@example.com', '1988-07-19', 'Győr', '2025-02-22 00:00:00', 'vbela', '$2b$10$S/oxPWR1GAefsAw1sksUtu9ghwQO.M1d9pOiQ3vX0Q5uQHSfbNqV2', 'visitor'),
+(6, 'Kovacs', 'Jeno', NULL, NULL, 'kovacsjeno@gmail.com', '2025-03-19', 'Saab', '2025-03-03 12:58:26', 'kjeno', '$2b$10$E4uY7aLRJ4uCeMCXKvDOj.Z6MzmiHVAm4mZKRLqhMcNywp8ihg8ou', 'visitor'),
+(7, 'Bela', 'Feri', NULL, NULL, 'belaferi@gmail.com', '2025-03-14', 'Budapest', '2025-03-03 14:09:27', 'bferi', '$2b$10$I/6jCKKXuCCqpoPBu7Qcy.fLhOjeaCYOpgTx267Eu3/Rzv7CyHTZu', 'coach'),
+(8, 'Kati', 'Bela', NULL, NULL, 'katibela@gmail.com', '2025-03-27', 'Szeged', '2025-03-03 14:10:52', 'kbela', '$2b$10$fBN1L4viZ4imJqPtzm61vOSBHLbyeLwsJsjv45h0sda/u7QTr2PSy', 'visitor');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sport`
+--
+
+CREATE TABLE `sport` (
+  `sport_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `sportnev` VARCHAR(45) DEFAULT NULL,
+  `leiras` VARCHAR(255) DEFAULT NULL,
+  `szabalyok` VARCHAR(555) DEFAULT NULL,
+  PRIMARY KEY (`sport_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sport`
 --
 
 INSERT INTO `sport` (`sport_id`, `sportnev`, `leiras`, `szabalyok`) VALUES
@@ -199,79 +225,15 @@ INSERT INTO `sport` (`sport_id`, `sportnev`, `leiras`, `szabalyok`) VALUES
 (8, 'MMA', 'Az MMA (Mixed Martial Arts) egy vegyes harcművészet, amely kombinálja a birkózást, boxot, jiu-jitsut és más stílusokat.\r\n          Az alábbiakban bemutatjuk a legfontosabb szabályokat:', '<ul>\r\n<li>A mérkőzés általában 3-5 menetből áll, egy menet 5 percig tart.</li>\r\n<li>Engedélyezett technikák: ütések, rúgások, bokszolás, fojtások, kulcsok és dobások.</li>\r\n<li>Tilos veszélyes technikák, például a hátsó fejre esést okozó dobások vagy tiltott területek támadása.</li>\r\n<li>A mérkőzést bírók figyelik, akik döntenek a szabályok betartásáról és a pontozásról.</li>\r\n<li>A győzelem kiütéssel, feladással, bírói döntéssel vagy diszkvalifikációval érhető el.</li>\r\n</ul>');
 
 --
--- Indexek a kiírt táblákhoz
+-- Indexes for dumped tables
 --
 
 --
--- A tábla indexei `adatmodositas`
---
-ALTER TABLE `adatmodositas`
-  ADD PRIMARY KEY (`modositas_id`),
-  ADD KEY `fr_user_id_idx` (`user_id`);
-
---
--- A tábla indexei `ertekelesek`
---
-ALTER TABLE `ertekelesek`
-  ADD PRIMARY KEY (`ertekeles_id`),
-  ADD KEY `fruserid_idx` (`user_id`),
-  ADD KEY `frsportklub_idx` (`sportklub_id`);
-
---
--- A tábla indexei `esemenyek`
---
-ALTER TABLE `esemenyek`
-  ADD PRIMARY KEY (`esemeny_id`),
-  ADD KEY `fr_user_id_idx` (`user_id`);
-
---
--- A tábla indexei `jelentkezes`
---
-ALTER TABLE `jelentkezes`
-  ADD PRIMARY KEY (`jelentkezes_id`),
-  ADD KEY `fk_user_id_idx` (`user_id`),
-  ADD KEY `fk_sportklub_id_idx` (`sportkulb_id`);
-
---
--- A tábla indexei `klubbok`
+-- AUTO_INCREMENT for table `klubbok`
 --
 ALTER TABLE `klubbok`
-  ADD PRIMARY KEY (`sprotklub_id`),
-  ADD KEY `fk_user_id_idx` (`user_id`),
-  ADD KEY `sportid` (`sport_id`),
-  ADD KEY `kb_vnev_idx` (`vnev`),
-  ADD KEY `kb_knev_idx` (`knev`);
+  MODIFY `sportklub_id` INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
---
--- A tábla indexei `latogatobejelentkezesek`
---
-ALTER TABLE `latogatobejelentkezesek`
-  ADD PRIMARY KEY (`bejelentkezes_id`),
-  ADD KEY `fk_latogatobejelentkezesek_latogatok` (`user_id`);
-
---
--- A tábla indexei `latogatok`
---
-ALTER TABLE `latogatok`
-  ADD PRIMARY KEY (`user_id`),
-  ADD KEY `vnevidx` (`vnev`),
-  ADD KEY `knevidx` (`knev`);
-
---
--- A tábla indexei `sport`
---
-ALTER TABLE `sport`
-  ADD PRIMARY KEY (`sport_id`);
-
---
--- A kiírt táblák AUTO_INCREMENT értéke
---
-
---
--- AUTO_INCREMENT a táblához `klubbok`
---
-ALTER TABLE `klubbok`
-  MODIFY `sprotklub_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
